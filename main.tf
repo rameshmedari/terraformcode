b05b62efe9f7bc2a5d1f9af6ef561a6004b03075
rameshmedari <63700872+rameshmedari@users.noreply.github.com>
2021-07-07 11:50:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 #Create security group with firewall rules
 resource "aws_security_group" "security_jenkins_port" {
-  key_name = "var.jenkins"
   name        = "security_jenkins_sg"
   description = "security group for jenkins"
 
